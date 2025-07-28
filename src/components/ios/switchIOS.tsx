cb2200d6afd716d10983bac54ae6dbac42056ecb
Unimorphic <86892331+unimorphic@users.noreply.github.com>
2025-07-27 22:38:56 -0600

    Remove Base UI

--- src/components/ios/switchIOS.tsx
+++ src/components/ios/switchIOS.tsx
@@ -1,62 +1,63 @@
-import { Switch } from "@base-ui-components/react";
 import { styled } from "@mui/material/styles";
-import { SwitchProps } from "@mui/material/Switch";
+import Switch, { switchClasses, SwitchProps } from "@mui/material/Switch";
 import { ReactNode } from "react";
 
 // https://www.sketch.com/s/f63aa308-1f82-498c-8019-530f3b846db9/symbols?g=Toggle
 
 const SwitchIOS: (props: SwitchProps) => ReactNode | Promise<ReactNode> =
-  styled(
-    (props: SwitchProps) => {
-      const { classes, type, value, ...otherProps } = props;
-      
-      // TODO handle slots
-
-      return (
-        <Switch.Root
-          type={type as "button"}
-          value={value as string}
-          {...otherProps}
-        >
-          <Switch.Thumb />
-        </Switch.Root>
-      );
-    },
-    { name: "AdaptiveSwitch", slot: "ios" },
-  )(({ theme }) => ({
-    width: 64,
+  styled((props: SwitchProps) => <Switch disableRipple {...props} />, {
+    name: "AdaptiveSwitch",
+    slot: "ios",
+  })(({ theme }) => ({
     height: 28,
-    borderRadius: 15,
-    backgroundColor: "rgba(120, 120, 120, 0.2)",
+    padding: 0,
+    width: 64,
 
-    ...theme.applyStyles("dark", {
-      backgroundColor: "rgba(120, 120, 128, 0.36)",
-    }),
+    [`& .${switchClasses.switchBase}`]: {
+      margin: 2,
+      padding: 0,
+    },
 
-    "&[data-checked]": {
-      backgroundColor: "rgba(52, 199, 89, 1)",
+    [`& .${switchClasses.track}`]: {
+      backgroundColor: "rgba(120, 120, 120, 0.2)",
+      borderRadius: 15,
+      opacity: 1,
 
+      transition: theme.transitions.create(["background-color"], {
+        duration: 200,
+      }),
       ...theme.applyStyles("dark", {
-        backgroundColor: "rgba(48, 209, 88, 1)",
+        backgroundColor: "rgba(120, 120, 128, 0.36)",
       }),
     },
 
-    "&[data-disabled]": {
-      opacity: 0.4,
-    },
-
-    "& > span": {
-      display: "block",
-      width: 39,
-      height: 24,
-      borderRadius: 13,
+    [`& .${switchClasses.thumb}`]: {
       backgroundColor: "#FFFFFF",
-      transition: theme.transitions.create(["translate"], {
-        duration: 150,
-      }),
+      borderRadius: 13,
+      boxShadow: "none",
+      display: "block",
+      height: 24,
+      width: 39,
+    },
 
-      "&[data-checked]": {
-        translate: "23px 0",
+    [`& .${switchClasses.checked}`]: {
+      [`& .${switchClasses.switchBase}`]: {
+        transform: "translateX(21px)",
+      },
+
+      [`& + .${switchClasses.track}`]: {
+        backgroundColor: "rgba(52, 199, 89, 1)",
+        opacity: 1,
+
+        ...theme.applyStyles("dark", {
+          backgroundColor: "rgba(48, 209, 88, 1)",
+        }),
+      },
+    },
+
+    [`& .${switchClasses.disabled}`]: {
+      [`& + .${switchClasses.track}, & .${switchClasses.thumb}`]: {
+        opacity: 0.4,
       },
     },
   }));
