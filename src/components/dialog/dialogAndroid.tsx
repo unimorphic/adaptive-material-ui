cb28a2e4e4ab5ed78b84e9db19f1ba5f5eb93bfe
Unimorphic <86892331+unimorphic@users.noreply.github.com>
2025-12-08 14:08:31 -0700

    MD3 Dialog

--- src/components/dialog/dialogAndroid.tsx
+++ src/components/dialog/dialogAndroid.tsx
@@ -1,5 +1,63 @@
+import { dialogClasses } from "@mui/material/Dialog";
+import { dialogActionsClasses } from "@mui/material/DialogActions";
+import { dialogContentClasses } from "@mui/material/DialogContent";
+import { dialogTitleClasses } from "@mui/material/DialogTitle";
+import { styled } from "@mui/material/styles";
+import composeClasses from "@mui/utils/composeClasses";
+import generateUtilityClass from "@mui/utils/generateUtilityClass";
+import { clsx } from "clsx";
 import { DialogResponsive, DialogResponsiveProps } from "./dialogResponsive";
 
+/**
+ * MD3 https://m3.material.io/components/dialogs
+ */
+const StyledDialogResponsive = styled(DialogResponsive, {
+  name: "AdaptiveDialog",
+  slot: "android",
+})<{ ownerState: DialogResponsiveProps }>(({ theme }) => {
+  const edgePadding = theme.spacing(3);
+
+  return {
+    [`& .${dialogClasses.paperFullScreen} .${dialogTitleClasses.root}`]: {
+      fontSize: theme.typography.pxToRem(22),
+      fontWeight: 400,
+      lineHeight: theme.typography.pxToRem(28),
+    },
+    [`& .${dialogClasses.paper}:not(.${dialogClasses.paperFullScreen})`]: {
+      borderRadius: 28,
+      maxWidth: 560,
+      minWidth: 280,
+
+      [`& .${dialogTitleClasses.root}`]: {
+        fontSize: theme.typography.pxToRem(24),
+        fontWeight: 400,
+        lineHeight: theme.typography.pxToRem(32),
+        paddingTop: edgePadding,
+      },
+      [`& .${dialogContentClasses.root}`]: {
+        paddingBottom: edgePadding,
+      },
+      [`& .${dialogActionsClasses.root}`]: {
+        padding: `0 ${edgePadding} ${edgePadding} ${edgePadding}`,
+      },
+    },
+  };
+});
+
 export function DialogAndroid(props: DialogResponsiveProps) {
-  return <DialogResponsive {...props} />;
+  const { className, ...otherProps } = props;
+
+  const composedClasses = composeClasses(
+    { android: ["android"] },
+    (s) => generateUtilityClass("AdaptiveDialog", s),
+    props.classes,
+  );
+
+  return (
+    <StyledDialogResponsive
+      className={clsx(composedClasses.android, className)}
+      ownerState={props}
+      {...otherProps}
+    />
+  );
 }
